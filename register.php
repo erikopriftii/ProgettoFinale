99097a4e601880e0cff5efc8919881d96a4ee201
erikopriftii <102923054+erikopriftii@users.noreply.github.com>
2024-04-13 12:34:53 +0200

    Add files via upload

---
+++ register.php
@@ -0,0 +1,45 @@
+<?php
+$host = "localhost";
+$db   = "Negozio";
+$username = "root";
+$pass = "";
+
+$conn = new mysqli($host, $username, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connessione fallita: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"]; 
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    
+
+    // Verifica se l'utente esiste già nel database
+    $stmt_check = $conn->prepare("SELECT * FROM users WHERE username = ?");
+    $stmt_check->bind_param("s", $username);
+    $stmt_check->execute();
+    $result = $stmt_check->get_result();
+
+    if ($result->num_rows > 0) {
+        echo "Questo username è già stato utilizzato.";
+    } else {
+        $hashed_password = md5($password); // Usando l'MD5 per criptare la password
+
+        
+        $stmt_insert = $conn->prepare("INSERT INTO users (username, password, email) VALUES (?, ?, ?)");
+        $stmt_insert->bind_param("sss", $username, $hashed_password, $email);
+
+        if ($stmt_insert->execute()) {
+            header("Location: template.html");
+            exit();
+        } else {
+            echo "Errore durante la registrazione: " . $conn->error;
+        }
+
+        $stmt_insert->close();
+    }
+
+    $stmt_check->close();
+}
